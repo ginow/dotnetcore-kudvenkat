1f08cf08514e8ba7f431b90ddf21b3ff2f6afcf8
Gino <gino.jameskutty@gmail.com>
2022-06-22 09:25:15 +0530

    serving default.html from wwwroot

--- EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -26,29 +26,14 @@ namespace EmployeeManagement
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.Use(async (context,next) =>
-            {
-                logger.LogInformation("MW1: Incoming request");
-                await next();
-                logger.LogInformation("MW1: Outgoing request");
-            });
-            app.Use(async (context, next) =>
-            {
-                logger.LogInformation("MW2: Incoming request");
-                await next();
-                logger.LogInformation("MW2: Outgoing request");
-            });
-          
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("MW3: Request handled and response produced");
-                logger.LogInformation("MW3: Request handled and response produced");
+                await context.Response.WriteAsync("Hello world  ");
+            
             });
         }
     }
