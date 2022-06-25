e09c0d0885817ad6d001d974dba1488b77e843ef
Gino <gino.jameskutty@gmail.com>
2022-06-25 07:50:43 +0530

    returning JSON

--- EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿namespace EmployeeManagement.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        public JsonResult Index()
         {
-            return "Hello from mvc";
+            return Json(new { id = 1, name = "parmgiv" });
         }
 
     }
--- EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -23,7 +23,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) // Dependency injection container
         {
-            services.AddMvc();
+           services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +38,7 @@ namespace EmployeeManagement
             app.UseMvcWithDefaultRoute();
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World");
+                await context.Response.WriteAsync("Hello World " + env.EnvironmentName);
 
             });
         }
