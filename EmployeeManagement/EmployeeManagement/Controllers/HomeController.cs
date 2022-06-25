1564c323bcc60c065de16a71b654c754a8ea4823
Gino <gino.jameskutty@gmail.com>
2022-06-25 16:10:11 +0530

    Return specific View

--- EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace EmployeeManagement.Controllers
         public ViewResult Details()
         {
             Employee model=_employeeRepositor.GetEmployee(1);
-            return View(model);
+            return View("Test");
         }
     }
 }
